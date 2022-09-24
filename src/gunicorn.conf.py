42be54e58cc0300f3966edbf91dfad970a893b03
mauwii <Mauwii@outlook.de>
2022-09-24 22:56:45 +0200

    update bind in gunicorn.conf.py

--- src/gunicorn.conf.py
+++ src/gunicorn.conf.py
@@ -6,7 +6,7 @@ name = "Gunicorn config for FastAPI"
 # accesslog = "./gunicorn-access.log"
 # errorlog = "./gunicorn-error.log"
 
-bind = str("0.0.0.0:" + os.getenv('PORT'))
+bind = str("0.0.0.0:" + str(os.getenv("PORT")))
 timeout = 600
 worker_class = "uvicorn.workers.UvicornWorker"
 workers = multiprocessing.cpu_count() * 2 + 1
