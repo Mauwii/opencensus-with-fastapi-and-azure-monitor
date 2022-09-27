deb546265761e125c1abeba84f6efd01b0e0e3e6
mauwii <Mauwii@outlook.de>
2022-09-27 09:08:41 +0200

    update entrypoint.sh to use persistent log path

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,24 +1,40 @@
 #!/usr/bin/env bash
 set -e
 
-echo "--------------------------------------------------------------"
-echo -e "\e[1;33m THIS CONTAINER IS ONLY INTENDED TO BE USED IN A AZURE WEBAPP\e[0m"
-echo "--------------------------------------------------------------"
+echo "--------------------------------------------------------"
+echo -e "\e[1;33m THIS CONTAINER IS INTENDED TO BE USED IN AZURE WEBAPPS\e[0m"
+echo "--------------------------------------------------------"
 
 echo "Starting SSH ..."
 service ssh start
 
+# persistent logs
+PERSIST_LOGS_DIR=/home/LogFiles
+PERSIST_LOGS=${PERSIST_LOGS_DIR}/gunicorn
+mkdir -p ${PERSIST_LOGS_DIR}
+touch ${PERSIST_LOGS}.log
+touch ${PERSIST_LOGS}-access.log
+touch ${PERSIST_LOGS}-error.log
+tail -n 0 -f ${PERSIST_LOGS}*.log &
+
+# [[ "$(python -c 'import multiprocessing; print(multiprocessing.cpu_count() * 2 + 1)')" -gt 12 ]] && workers=12
 echo "Starting fastapi ..."
-[[ "$(python -c 'import multiprocessing; print(multiprocessing.cpu_count() * 2 + 1)')" -gt 12 ]] && workers=12
 gunicorn \
-  --worker-class uvicorn.workers.UvicornH11Worker \
-  --workers "${workers:-$(python -c 'import multiprocessing; print(multiprocessing.cpu_count() * 2 + 1)')}" \
-  --access-logfile '-' \
-  --error-logfile '-' \
-  --bind="0.0.0.0:${WEBSITES_PORT:-8080}" \
-  --forwarded-allow-ips "${WEBSITE_PRIVATE_IP:-127.0.0.1}" \
-  --timeout=600 \
+  --proxy-protocol \
+  --bind "0.0.0.0:${WEBSITES_PORT:-8080}" \
+  --name "${WEBSITE_SITE_NAME:-fastapi-opencensus}" \
+  --forwarded-allow-ips "${WEBSITE_SITE_NAME:-${HOSTNAME:-127.0.0.1}}" \
+  --worker-class uvicorn.workers.UvicornWorker \
+  --workers 4 \
+  --log-level "${LOGLEVEL:-info}" \
+  --log-file "${PERSIST_LOGS}.log" \
+  --access-logfile "${PERSIST_LOGS}-access.log" \
+  --error-logfile "${PERSIST_LOGS}-error.log" \
+  --timeout 600 \
+  --chdir /app \
   'main:app'
+  # --forwarded-allow-ips "${WEBSITE_PRIVATE_IP:-127.0.0.1}" \
+  # --forwarded-allow-ips="${WEBSITE_PRIVATE_IP:-127.0.0.1}" \
 #  --workers ${workers:-$(python -c 'import multiprocessing; print(multiprocessing.cpu_count() * 2 + 1)')} \
 
 # uvicorn main:app \
