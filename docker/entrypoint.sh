d9cbad1b7336a17232358c63b5d06e0d3c8aaa99
mauwii <Mauwii@outlook.de>
2022-09-27 02:49:50 +0200

    update entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -9,9 +9,22 @@ echo "Starting SSH ..."
 service ssh start
 
 echo "Starting fastapi ..."
-# python main.py
-uvicorn main:app \
-  --port "${WEBSITES_PORT:-8080}" \
-  --host="0.0.0.0" \
-  --log-level="info" \
-  --workers="${workers:-$(python -c 'import multiprocessing; print(multiprocessing.cpu_count())')}"
+[[ "$(python -c 'import multiprocessing; print(multiprocessing.cpu_count() * 2 + 1)')" -gt 12 ]] && workers=12
+gunicorn \
+  --worker-class uvicorn.workers.UvicornH11Worker \
+  --workers "${workers:-$(python -c 'import multiprocessing; print(multiprocessing.cpu_count() * 2 + 1)')}" \
+  --access-logfile '-' \
+  --error-logfile '-' \
+  --bind="0.0.0.0:${WEBSITES_PORT:-8080}" \
+  --forwarded-allow-ips "${WEBSITE_PRIVATE_IP:-127.0.0.1}" \
+  --timeout=600 \
+  'main:app'
+#  --workers ${workers:-$(python -c 'import multiprocessing; print(multiprocessing.cpu_count() * 2 + 1)')} \
+
+# uvicorn main:app \
+#   --port "${WEBSITES_PORT:-8080}" \
+#   --host="0.0.0.0" \
+#   --log-level="info" \
+#   --proxy-headers \
+#   --forwarded-allow-ips "${WEBSITE_PRIVATE_IP:-127.0.0.1}" \
+#   --workers="${workers:-$(python -c 'import multiprocessing; print(multiprocessing.cpu_count() * 2 + 1)')}"
